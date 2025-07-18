20f2f5e869c713f4d45a0e1b9bc42da9cb9576ed
LucKer58 <lucien.kern@bluewin.ch>
2025-07-18 17:10:30 +0200

    collected raw data from pubmed

--- src/data_collector.py
+++ src/data_collector.py
@@ -1,13 +1,14 @@
 import requests
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from config.specialties import MEDICAL_SPECIALTIES
-# Add other imports you need
+import json
 
 class PubMedDataCollector:
 
 
-    def __init__(self, speciality):
-        
-        self.speciality = speciality
+    def __init__(self):
         self.base_url = 'https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi'
 
     
@@ -33,15 +34,23 @@ class PubMedDataCollector:
         
         return response.text
     
-    def collect_for_specialty(self, specialty_name):
-        mesh_terms = MEDICAL_SPECIALTIES[specialty_name]['mesh_terms']
-        abstracts_per_term = 50 // len(mesh_terms)
-    
-        all_ids = []
-        for mesh_term in mesh_terms:
-            ids = self.search_papers(mesh_term, abstracts_per_term)
-            all_ids.extend(ids)
+    def collect_for_specialties(self):
+
+        for specialty_name in MEDICAL_SPECIALTIES.keys():
+            mesh_terms = MEDICAL_SPECIALTIES[specialty_name]['mesh_terms']
+            abstracts_per_term = 50 // len(mesh_terms)
         
-        # Actually get the abstracts
-        abstracts = self.get_abstracts(all_ids)
-        return abstracts
+            all_ids = []
+            for mesh_term in mesh_terms:
+                ids = self.search_papers(mesh_term, abstracts_per_term)
+                all_ids.extend(ids)
+            
+            abstracts = self.get_abstracts(all_ids)
+            data = {'speciality': specialty_name, 'abstracts': abstracts}
+            with open(f'data/raw/{specialty_name}_abstracts.json', 'w') as f:
+ 
+                json.dump(data, f)
+
+if __name__ == "__main__":
+    collector = PubMedDataCollector()
+    collector.collect_for_specialties()
